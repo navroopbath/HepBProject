945ee01f659444e6aeecf0fafa238443c2ca14ad
Patricia Demorest <p.demorest@berkeley.edu>
2015-03-12 00:06:47 -0700

    flash notices failing, need to implement invalid fields scenario

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -45,7 +45,31 @@ class MembersController < ApplicationController
   end
 
   def sign_up
-    redirect_to members_login_index_path
+    if params[:members][:password] == params[:members][:confirm_password] 
+      if not empty_fields?
+        flash[:notice] = 'You have been sent a confirmation email'
+        redirect_to members_login_index_path, notice: 'You have been sent a confirmation email' and return
+      else
+        flash[:notice] = 'One or more of the fields are empty'
+        #session[:members]
+        flash.keep
+        redirect_to members_sign_up_index_path and return
+      end
+    else
+      flash[:notice] = 'Passwords do not match'
+      # session[:members]
+      flash.keep
+      redirect_to members_sign_up_index_path and return
+    end
+  end
+
+  def empty_fields?
+    params[:members].each do |field| 
+      if field[1] == nil or field[1] == '' 
+        return true
+      end
+    end
+    return false
   end
 
 end
