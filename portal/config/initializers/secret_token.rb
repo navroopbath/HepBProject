6d94c7d67f2568a0357f281698265fdd401e81cb
Navroop Singh <roopsingh93@gmail.com>
2015-03-11 19:20:08 -0700

    Commit for 1st Heroku deployment.

--- portal/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Portal::Application.config.secret_token = '14c55e7685b78ca8bab16fb5638db89d5a63e2ed69c63b074168c741c5365d910d9c27061ebb6fbd872669220200dc1615244916b603c439f72df7ee0b9c96d1'
