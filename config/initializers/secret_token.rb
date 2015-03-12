9c9dfbdeb8331b520bb3cac805e1223ae80a68cc
ShannonAxelrod <shanaxel42@berkeley.edu>
2015-03-11 21:22:29 -0700

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HepBProject::Application.config.secret_token = 'b27e2efda74183d6b65b16c2bcabf21b32fd6eb3b087785c4670de337c4dfdee999cdd7d0d676c8f8c694d9a32ac14950f8bea36b2df299231a5c43548d601f7'
