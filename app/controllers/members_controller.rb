0ca4912afaa4e75a66af92e0c9ac4a8288a60b07
ShannonAxelrod <shanaxel42@berkeley.edu>
2015-03-11 22:02:41 -0700

    added model/veiw/controllers

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,50 @@
+class MembersController < ApplicationController
+
+  def show
+  	id = params[:id]
+  	@member = Member.find(id)
+  end
+
+  def create
+  	@member = Member.create!(params[:member])
+  	flash[:notice] = "You have signed up with username: #{@member.email}"
+  	# redirect_to 'not_sure'
+  end
+
+  def edit
+  	@member = Member.find(params[:id])
+  end
+
+  def new
+  	# render new view
+  end
+
+  def update
+  	@member = Member.find(params[:id])
+  	@member.update_attributes!(params[:member])
+  	flash[:notice] = "#{@member.first_name} your information was successfully updated"
+  	# redirect_to 'not sure'
+  end
+
+  def destroy
+  	@member = Member.find(params[:id])
+  	@member.destroy
+  	flash[:notice] = "Member #{@member.first_name} #{@member.last_name} was destroyed"
+  	# redirect_to 'not sure'
+  end
+
+  def login_index
+  end
+
+  def login
+  
+  end
+
+
+  def sign_up_index
+  end
+
+  def sign_up
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+members = [{:first_name =>'Navroop', :last_name =>'Singh', :year =>'Senior', :phone => '4084766051',:email =>'roopsingh93@berkeley.edu', :password =>'test', :is_admin =>'true', :hours =>'5'},
+        {:first_name =>'Mehdi', :last_name =>'Nazemi', :year =>'Senior', :phone =>'4084556242',:email =>'medn94@berkeley.edu', :password =>'test', :is_admin =>'true', :hours =>'5'},
+        {:first_name =>'Patricia', :last_name =>'Demorest', :year =>'Senior', :phone =>'7608555141',:email =>'p.demorest@berkeley.edu', :password =>'test', :is_admin =>'true', :hours =>'5'},
+        {:first_name =>'Shannon', :last_name =>'Axelrod', :year =>'Senior', :phone =>'4258909005',:email =>'shannonaxelrod@gmail.com', :password =>'test', :is_admin =>'true', :hours =>'5'},
+        {:first_name =>'John', :last_name =>'Russel', :year =>'Senior', :phone =>'2062184949',:email =>'john6@berkeley.edu', :password =>'test', :is_admin =>'true', :hours =>'5'},
+        {:first_name =>'Michael', :last_name =>'Rowe', :year =>'Senior', :phone =>'5555555555',:email =>'michaelrowe@berkeley.edu', :password =>'test', :is_admin =>'true', :hours =>'5'},
+        {:first_name =>'Armando', :last_name =>'Fox', :year =>'Super-Senior', :phone =>'1011100101',:email =>'afox@hotmail.com', :password =>'saas', :is_admin =>'false', :hours =>'100'},
+        {:first_name =>'Craig', :last_name =>'Hiller', :year =>'Senior', :phone =>'5555555555',:email =>'chiller@berkeley.edu', :password =>'saas', :is_admin =>'false', :hours =>'50'}
+]
+
+# events = [{:name => 'Korean Health Fair', :date => '3/27/2015', :location => 'Sproul Hall', :description => 'Open Admission', :start_time => '14:00', :duration => '2:00', :num_volunteers => '4', :num_waitlist => '3'},
+#         {:name => 'Chinese Health Fair', :date => '4/20/2015', :location => 'Sproul Hall', :description => 'Open Admission', :start_time => '14:00', :duration => '2:00', :num_volunteers => '4', :num_waitlist => '2'},
+#         {:name => 'Japanese Health Fair', :date => '5/17/2015', :location => 'Sproul Hall', :description => 'Open Aadmission', :start_time => '14:00', :duration => '2:00', :num_volunteers => '3', :num_waitlist => '0'}
+# ]
+
+members.each do |member|
+  Member.create!(member)
+end
+
+# events.each do |event|
+#     Event.create!(event)
+# end
