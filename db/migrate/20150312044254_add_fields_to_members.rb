c0dd47553c4afa8f464ce9f451c40465c49fe517
Navroop Singh <roopsingh93@gmail.com>
2015-03-13 18:04:21 -0700

    changes the phone attribute to a float

--- db/migrate/20150312044254_add_fields_to_members.rb
+++ db/migrate/20150312044254_add_fields_to_members.rb
@@ -1,13 +1,22 @@
 class AddFieldsToMembers < ActiveRecord::Migration
   def change
-    create_table :members
-    add_column :members, :first_name, :string
-    add_column :members, :last_name, :string
-    add_column :members, :year, :string
-    add_column :members, :phone, :int8, :limit => 8
-    add_column :members, :email, :string
-    add_column :members, :password, :string
-    add_column :members, :is_admin, :boolean
-    add_column :members, :hours, :int
+    create_table :members do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :year
+      t.float :phone
+      t.string :email
+      t.string :password
+      t.boolean :is_admin
+      t.integer :hours
+    end
+    # add_column :members, :first_name, :string
+    # add_column :members, :last_name, :string
+    # add_column :members, :year, :string
+    # add_column :members, :phone, :bigint
+    # add_column :members, :email, :string
+    # add_column :members, :password, :string
+    # add_column :members, :is_admin, :boolean
+    # add_column :members, :hours, :int
   end
 end
