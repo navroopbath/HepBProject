e03b2428c1becb4e35799bff43f5a0b070a5dfd6
pdemorest <p.demorest@berkeley.edu>
2015-04-03 15:17:45 -0700

    fixed typo in assert statement

--- features/step_definitions/stats_steps.rb
+++ features/step_definitions/stats_steps.rb
@@ -3,5 +3,5 @@ Then /I should see "(.*)" under "(.*)"/ do |e1, e2|
   #  page.body is the entire content of the page as a string.
   first = page.body.index(e1) or return false
   second = page.body.index(e2) or return false
-  assert(first = second -1, "The movies were not alphabetic")
+  assert(first = second -1, "The events were not chronological")
 end
