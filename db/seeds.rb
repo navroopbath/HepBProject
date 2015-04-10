37479467acaa9fedd90a4db52f1fefc4d65a4f6c
Patricia Demorest <p.demorest@berkeley.edu>
2015-04-10 15:16:48 -0700

    fixed seeds so passwords are min 8 characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-members = [{:first_name =>'Navroop', :last_name =>'Singh', :grad_date =>'2002-05-30T09:00:00', :phone => '4084766051',:email =>'roopsingh93@berkeley.edu', :password =>'test', :is_admin =>'true', :warnings =>'0'},
-        {:first_name =>'Mehdi', :last_name =>'Nazemi', :grad_date =>'2002-05-30T09:00:00', :phone =>'4084556242',:email =>'medn94@berkeley.edu', :password =>'test', :is_admin =>'true', :warnings =>'0'},
-        {:first_name =>'Patricia', :last_name =>'Demorest', :grad_date =>'2002-05-30T09:00:00', :phone =>'7608555141',:email =>'p.demorest@berkeley.edu', :password =>'test', :is_admin =>'true', :warnings =>'0'},
-        {:first_name =>'John', :last_name =>'Russel', :grad_date =>'2002-05-30T09:00:00', :phone =>'2062184949',:email =>'john6@berkeley.edu', :password =>'test', :is_admin =>'true', :warnings =>'0'},
-        {:first_name =>'Michael', :last_name =>'Rowe', :grad_date =>'2002-05-30T09:00:00', :phone =>'5555555555',:email =>'michaelrowe@berkeley.edu', :password =>'test', :is_admin =>'true', :warnings =>'0'},
-        {:first_name =>'Armando', :last_name =>'Fox', :grad_date =>'2002-05-30T09:00:00', :phone =>'1011100101',:email =>'afox@hotmail.com', :password =>'saas', :is_admin =>'false', :warnings =>'0'},
-        {:first_name =>'Craig', :last_name =>'Hiller', :grad_date =>'2002-05-30T09:00:00', :phone =>'5555555555',:email =>'chiller@berkeley.edu', :password =>'saas', :is_admin =>'false', :warnings =>'0'}
+members = [{:first_name =>'Navroop', :last_name =>'Singh', :grad_date =>'2002-05-30T09:00:00', :phone => '4084766051',:email =>'roopsingh93@berkeley.edu', :password =>'testtest', :is_admin =>'true', :warnings =>'0'},
+        {:first_name =>'Mehdi', :last_name =>'Nazemi', :grad_date =>'2002-05-30T09:00:00', :phone =>'4084556242',:email =>'medn94@berkeley.edu', :password =>'testtest', :is_admin =>'true', :warnings =>'0'},
+        {:first_name =>'Patricia', :last_name =>'Demorest', :grad_date =>'2002-05-30T09:00:00', :phone =>'7608555141',:email =>'p.demorest@berkeley.edu', :password =>'testtest', :is_admin =>'true', :warnings =>'0'},
+        {:first_name =>'John', :last_name =>'Russel', :grad_date =>'2002-05-30T09:00:00', :phone =>'2062184949',:email =>'john6@berkeley.edu', :password =>'testtest', :is_admin =>'true', :warnings =>'0'},
+        {:first_name =>'Michael', :last_name =>'Rowe', :grad_date =>'2002-05-30T09:00:00', :phone =>'5555555555',:email =>'michaelrowe@berkeley.edu', :password =>'testtest', :is_admin =>'true', :warnings =>'0'},
+        {:first_name =>'Armando', :last_name =>'Fox', :grad_date =>'2002-05-30T09:00:00', :phone =>'1011100101',:email =>'afox@hotmail.com', :password =>'saassaas', :is_admin =>'false', :warnings =>'0'},
+        {:first_name =>'Craig', :last_name =>'Hiller', :grad_date =>'2002-05-30T09:00:00', :phone =>'5555555555',:email =>'chiller@berkeley.edu', :password =>'saassaas', :is_admin =>'false', :warnings =>'0'}
 ]
 
 events = [{:event_name => 'Korean Health Fair', :date => '2015-04-15T10:00:00', :location => 'Sproul Hall', :description => 'Open Admission', :duration => '2:00', :num_volunteers => '10'},
