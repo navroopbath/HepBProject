c788e70c46a8b4767a08a7e398630c36f4927e3f
Patricia Demorest <p.demorest@berkeley.edu>
2015-04-24 16:08:44 -0700

    personalized subject line and email body for confirmation email

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,8 +1,9 @@
 class MemberMailer < ActionMailer::Base
-  default from: "sandbox60b4290575ab49f386dd50fc026416a2.mailgun.org"
+  default from: "noreply@hepbproject.com"
 
   def registration_confirmation(member)
     @member = member
     mail(:to => "#{member.email}", :subject => "Welcome to Hep B Portal")
   end
+
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.mailgun.org",
-  :port                 => 25,
+  :port                 => "587",
   :domain               => "mailgun.org",
-  :user_name            => "sandbox60b4290575ab49f386dd50fc026416a2.mailgun.org",
+  :user_name            => "postmaster@sandbox60b4290575ab49f386dd50fc026416a2.mailgun.org",
   :password             => "8c8238822e15be46e138d3974cde9129",
   :authentication       => "plain",
   :enable_starttls_auto => true
