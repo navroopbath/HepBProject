8e345674bfbe75d1267200e405674cb3c5ae104a
Navroop Singh <roopsingh93@gmail.com>
2015-04-24 17:02:58 -0700

    add functionality for signing up for event. checks if user is already signed up.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -33,6 +33,27 @@ class EventsController < ApplicationController
     @event.destroy
   end
 
+  def signup
+    @event = Event.where(id: params[:id])[0]
+    if Memevent.where(member_id: current_member.id, event_id: @event.id).length > 0
+      flash[:error] = "You have already signed up for this event."
+    else
+      if @event.memevents.length > @event.num_volunteers
+        waitlisted = 'true'
+      else
+        waitlisted = 'false'
+      end
+      Memevent.create(:event_id => @event.id, :member_id => current_member.id, :waitlisted => waitlisted, :date_added => Time.now.utc, :approved => 'true', :hours => @event.duration)
+      waitlisted == 'true' ? flash[:error] = "You have been waitlisted for #{@event.event_name}. You'll receive an email notification if you are moved into the volunteer list." : flash[:error] = "You have successfully signed up for #{@event.event_name}."
+    end
+    redirect_to events_index_path
+  end
+
+  # private
+  # def already_signed_up_for_event(event)
+  #   if Memevent.where(mem)
+
+
 #   def create
 #     @event = Event.create!(params[:event])
 #     flash[:notice] = "You have signed up with username: #{@event.name}"
