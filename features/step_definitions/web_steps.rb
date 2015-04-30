96f9d094c4e50b27014ef05da0e21600225dbe83
ShannonAxelrod <shanaxel42@berkeley.edu>
2015-04-30 14:11:14 -0700

    passing all cuc tests just need last step for javascript

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -77,6 +77,9 @@ end
 # based on naming conventions.
 #
 
+When(/^I select "(.*?)" for "(.*?)"$/) do |arg1, arg2|
+  select arg1, :from => arg2
+end
 
 When /^(?:|I )fill in the following:$/ do |fields|
   fields.rows_hash.each do |name, value|
