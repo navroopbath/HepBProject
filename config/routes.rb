4939f77dfdded72826eae1ec8909d3a509c602e3
Patricia Demorest <p.demorest@berkeley.edu>
2015-05-01 18:03:02 -0700

    refactored so admin settings now in own controller"

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ HepBProject::Application.routes.draw do
   get 'members/:id/announcements', to: 'members#announcements', as: 'members_announcements'
   post 'members/:id/announcements', to: 'members#announcements', as: 'members_announcements'
   get 'members/:id/stats', to: 'members#stats', as: 'members_stats'
-  get 'members/:id/admin_settings', to: 'members#admin_settings', as: 'members_admin_settings'
-  put 'members/:id/admin_settings', to:'members#update_admin_settings', as: 'members_update_admin_settings'
+  get 'settings/:id', to: 'settings#index', as: 'settings_index'
+  put 'settings/:id', to:'settings#update', as: 'settings_update'
 
   resources :members do
     delete :delete_announ, :on => :member
